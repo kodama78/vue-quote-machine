d182ed3c3140d66661927f25d0b2a2dc397291c8
Shawn Otomo <kodama78@gmail.com>
2019-07-07 21:32:07 -0700

    quote maker project

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <Header :maxQuotes="maxQuotes" :quoteCount="quotes.length"/>
+        <NewQuote @quoteAdded="newQuote"></NewQuote>
+        <QuoteGrid :quotesArray="quotes" @quoteDeleted="deleteQuote"></QuoteGrid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to delete it</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+
+    export default {
+        data: function () {
+         return {
+           maxQuotes: 10,
+           quotes: ['First quote']
+         }
+        },
+        methods: {
+          newQuote(quote) {
+            if(this.quotes.length >= this.maxQuotes) {
+                return alert('Please delete Quotes first')
+            };
+            this.quotes.push(quote);
+          },
+          deleteQuote(index) {
+            this.quotes.splice(index, 1);
+          }
+        },
+        components: {
+            QuoteGrid,
+            NewQuote,
+            Header
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="row">
+		<Quote v-for="(quote, index) in quotesArray" @click.native="deleteQuote(index)">{{ quote }}</Quote>
+	</div>
+</template>
+
+<script>
+	import Quote from "./Quote.vue";
+
+	export default {
+		components: {
+			Quote
+		},
+		name: "QuoteGrid",
+		props: ['quotesArray'],
+		methods: {
+			deleteQuote(index) {
+				this.$emit('quoteDeleted', index);
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
